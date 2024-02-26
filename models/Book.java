380b852e0d0e2fe4bacf326a991017ebaddaa11e
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2024-02-26 13:02:46 +0100

    ✅ ADDED: Book model file

---
+++ models/Book.java
@@ -0,0 +1,47 @@
+package models;
+
+public class Book {
+  // MARK I - Properties
+  private String title;
+  private String author;
+  private String isbn;
+
+  // MARK II - Constructors
+  public Book() {
+    this.title = "";
+    this.author = "";
+    this.isbn = "";
+  }
+
+  public Book(String title, String author, String isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+
+  // MARK III - Getters
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  // MARK IV - Setters
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+}
